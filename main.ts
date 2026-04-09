890cb7cd8eea5e5a3d6b5813405227679ecc734a
Lara <99083706+kammererlara@users.noreply.github.com>
2026-04-09 11:02:22 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -20,13 +20,26 @@ function ergebnisKorrekt (loesung: number, angabe: number) {
     }
 }
 function wandleUmInDezimal (binär: number) {
-	
+    ergebnis = 0
+    durchlauf = 0
+    while (binär > 0) {
+        ergebnis += binär % 10 * 2 ** durchlauf
+        basic.showNumber(ergebnis)
+        durchlauf += 1
+    }
 }
 radio.onReceivedString(function (receivedString) {
     basic.showNumber(parseFloat(receivedString))
     wandleUmInDezimal(parseFloat(receivedString))
     ergebnisKorrekt(1, 1)
 })
+input.onButtonPressed(Button.B, function () {
+    dezimal = randint(0, 10)
+    basic.showNumber(dezimal)
+    wandleUmInDezimal(dezimal)
+})
+let durchlauf = 0
+let ergebnis = 0
 let dezimal = 0
-radio.setGroup(191)
+radio.setGroup(190)
 erzeugeAngabe()
