85328bf1e7296b7dabfd29a61d85ad1419fd18d3
Lara <99083706+kammererlara@users.noreply.github.com>
2026-04-09 12:25:51 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,55 @@
+function wandleDezimalInHexadezimal (dezimal: number) {
+    hex2 = ""
+    durchlauf = 0
+    while (dezimal > 0) {
+        hex2 = "" + wandleDezimalInHexadezimalStelle(dezimal % 16) + hex2
+        durchlauf += 1
+        dezimal /= 16
+dezimal = Math.round(dezimal)
+    }
+    return hex2
+}
+function wandleDezimalInHexadezimalStelle (dezimal: number) {
+    if (dezimal == 10) {
+        return "A"
+    } else if (dezimal == 11) {
+        return "B"
+    } else if (dezimal == 12) {
+        return "C"
+    } else if (dezimal == 13) {
+        return "D"
+    } else if (dezimal == 14) {
+        return "E"
+    } else if (dezimal == 15) {
+        return "F"
+    } else {
+        return convertToText(dezimal)
+    }
+}
 function erzeugeAngabe () {
     dezimal = randint(0, 31)
-    basic.showNumber(dezimal)
-    radio.sendString(convertToText(dezimal))
-    basic.showLeds(`
-        . . # . .
-        . # # # .
-        # . # . #
-        . . # . .
-        . . # . .
-        `)
+    if (basis == 0) {
+        basic.showNumber(dezimal)
+        radio.sendString(convertToText(dezimal))
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else {
+        basic.showNumber(dezimal)
+        basic.showString("" + (wandleDezimalInHexadezimal(dezimal)))
+        radio.sendString("" + (wandleDezimalInHexadezimal(dezimal)))
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
 }
 input.onButtonPressed(Button.A, function () {
     erzeugeAngabe()
@@ -16,27 +57,16 @@ input.onButtonPressed(Button.A, function () {
 function ergebnisKorrekt (loesung: number, angabe: number) {
     if (loesung == angabe) {
         basic.showIcon(IconNames.Yes)
-        return "korrekt"
+        return 1
     } else {
         basic.showIcon(IconNames.No)
-        return "falsch"
+        return 0
     }
-}
-function wandleUmInDezimal (binaer: number) {
-    ergebnis = 0
-    durchlauf = 0
-    while (binaer > 0) {
-        ergebnis += binaer % 10 * 2 ** durchlauf
-        durchlauf += 1
-        binaer /= 10
-binaer = Math.round(binaer)
-    }
-    return ergebnis
 }
 radio.onReceivedString(function (receivedString) {
     basic.showNumber(parseFloat(receivedString))
-    basic.showNumber(wandleUmInDezimal(parseFloat(receivedString)))
-    radio.sendString("" + (ergebnisKorrekt(wandleUmInDezimal(parseFloat(receivedString)), dezimal)))
+    basic.showNumber(wandleBinaerInDezimal(parseFloat(receivedString)))
+    radio.sendString("" + (ergebnisKorrekt(wandleBinaerInDezimal(parseFloat(receivedString)), dezimal)))
 })
 input.onButtonPressed(Button.B, function () {
     basis += 1
@@ -47,10 +77,22 @@ input.onButtonPressed(Button.B, function () {
         basic.showNumber(16)
     }
 })
-let basis = 0
-let binaer = 0
-let durchlauf = 0
+function wandleBinaerInDezimal (binaer: number) {
+    ergebnis = 0
+    durchlauf = 0
+    while (binaer > 0) {
+        ergebnis += binaer % 10 * 2 ** durchlauf
+        durchlauf += 1
+        binaer /= 10
+binaer = Math.round(binaer)
+    }
+    return ergebnis
+}
 let ergebnis = 0
+let basis = 0
+let durchlauf = 0
+let hex2 = ""
+let binaer = 0
 let dezimal = 0
-radio.setGroup(191)
+radio.setGroup(190)
 erzeugeAngabe()
