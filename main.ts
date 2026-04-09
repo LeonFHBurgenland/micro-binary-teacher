4a189dbd2f9df9e98e8fbea730fd38876421cda7
Lara <99083706+kammererlara@users.noreply.github.com>
2026-04-09 10:40:45 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,32 @@
-input.onButtonPressed(Button.A, function () {
-    dezimal = randint(0, 10)
+function erzeugeAngabe () {
+    dezimal = randint(0, 255)
     basic.showNumber(dezimal)
     radio.sendNumber(dezimal)
+}
+input.onButtonPressed(Button.A, function () {
+    erzeugeAngabe()
 })
+function ergebnisKorrekt (loesung: number, angabe: number) {
+    if (loesung == angabe) {
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            . . . . .
+            `)
+    } else {
+        basic.showString("X")
+    }
+}
+function wandleUmInDezimal (binär: number) {
+	
+}
 radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
+    basic.showNumber(parseFloat(receivedString))
+    wandleUmInDezimal(parseFloat(receivedString))
+    ergebnisKorrekt(1, 1)
 })
 let dezimal = 0
 radio.setGroup(191)
-dezimal = randint(0, 10)
-basic.showNumber(dezimal)
-radio.sendNumber(dezimal)
+erzeugeAngabe()
