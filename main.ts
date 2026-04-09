f659e64ebac5089c3393ebd03aab8a4b23a13525
Lara <99083706+kammererlara@users.noreply.github.com>
2026-04-09 10:17:00 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    dezimal = randint(0, 10)
+    basic.showNumber(dezimal)
+    radio.sendNumber(dezimal)
 })
+let dezimal = 0
+radio.setGroup(191)
+dezimal = randint(0, 10)
+basic.showNumber(dezimal)
+radio.sendNumber(dezimal)
